da2b639496bc2d3f57809979bf18b0ffae783eed
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2026-01-21 12:47:51 +0100

    [WIP] peripheral_nfc_pairing: use BM BM_NFC_NDEF_LE_OOB_REC
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

--- samples/bluetooth/peripheral_nfc_pairing/src/main.c
+++ samples/bluetooth/peripheral_nfc_pairing/src/main.c
@@ -9,8 +9,11 @@
 
 #include <nfc_t4t_lib.h>
 #include <nfc/t4t/ndef_file.h>
+
 #include <nfc/ndef/msg.h>
-#include <nfc/ndef/text_rec.h>
+#include <nfc/ndef/ch.h>
+#include <bm/nfc/ndef/ch_msg.h>
+#include <bm/nfc/ndef/le_oob_rec.h>
 
 #include <bm/softdevice_handler/nrf_sdh.h>
 #include <bm/softdevice_handler/nrf_sdh_ble.h>
@@ -63,27 +66,9 @@ static uint16_t conn_handle = BLE_CONN_HANDLE_INVALID;
 static uint16_t peer_id;
 
 
-/* Text message in English with its language code. */
-static const uint8_t en_payload[] = {
-	'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'
-};
-static const uint8_t en_code[] = {'e', 'n'};
-
-/* Text message in Norwegian with its language code. */
-static const uint8_t no_payload[] = {
-	'H', 'a', 'l', 'l', 'o', ' ', 'V', 'e', 'r', 'd', 'e', 'n', '!'
-};
-static const uint8_t no_code[] = {'N', 'O'};
-
-/* Text message in Polish with its language code. */
-static const uint8_t pl_payload[] = {
-	'W', 'i', 't', 'a', 'j', ' ', 0xc5, 0x9a, 'w', 'i', 'e', 'c', 'i',
-	'e', '!'
-};
-static const uint8_t pl_code[] = {'P', 'L'};
-
 /* Buffer used to hold an NFC NDEF message. */
 static uint8_t ndef_msg_buf[NDEF_MSG_BUF_SIZE];
+
 
 static void led_init(void)
 {
@@ -128,72 +113,7 @@ static void nfc_callback(void *context,
  */
 static int welcome_msg_encode(uint8_t *buffer, uint32_t *len)
 {
-	int err;
-	uint32_t ndef_len = nfc_t4t_ndef_file_msg_size_get(*len);
-
-	/* Create NFC NDEF text record description in English */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_en_text_rec,
-				      UTF_8,
-				      en_code,
-				      sizeof(en_code),
-				      en_payload,
-				      sizeof(en_payload));
-
-	/* Create NFC NDEF text record description in Norwegian */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_no_text_rec,
-				      UTF_8,
-				      no_code,
-				      sizeof(no_code),
-				      no_payload,
-				      sizeof(no_payload));
-
-	/* Create NFC NDEF text record description in Polish */
-	NFC_NDEF_TEXT_RECORD_DESC_DEF(nfc_pl_text_rec,
-				      UTF_8,
-				      pl_code,
-				      sizeof(pl_code),
-				      pl_payload,
-				      sizeof(pl_payload));
-
-	/* Create NFC NDEF message description, capacity - MAX_REC_COUNT
-	 * records
-	 */
-	NFC_NDEF_MSG_DEF(nfc_text_msg, MAX_REC_COUNT);
-
-	/* Add text records to NDEF text message */
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_en_text_rec));
-	if (err < 0) {
-		LOG_ERR("Cannot add first record!");
-		return err;
-	}
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_no_text_rec));
-	if (err < 0) {
-		LOG_ERR("Cannot add second record!");
-		return err;
-	}
-	err = nfc_ndef_msg_record_add(&NFC_NDEF_MSG(nfc_text_msg),
-				   &NFC_NDEF_TEXT_RECORD_DESC(nfc_pl_text_rec));
-	if (err < 0) {
-		LOG_ERR("Cannot add third record!");
-		return err;
-	}
-
-	err = nfc_ndef_msg_encode(&NFC_NDEF_MSG(nfc_text_msg),
-				  nfc_t4t_ndef_file_msg_get(buffer),
-				  &ndef_len);
-	if (err < 0) {
-		LOG_ERR("Cannot encode message!");
-		return err;
-	}
-
-	err = nfc_t4t_ndef_file_encode(buffer, &ndef_len);
-	if (err) {
-		LOG_ERR("Cannot encode NDEF file!");
-		return err;
-	}
-	*len = ndef_len;
+	int err = 0;
 
 	return err;
 }
